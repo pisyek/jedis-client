47d418a92ea7d4cdb7580124c8532241ac799876
Pisyek <pisyek@gmail.com>
2022-01-12 14:45:09 +0800

    add comment and function to add key value in redis

--- src/RedisClient.java
+++ src/RedisClient.java
@@ -12,6 +12,11 @@ public class RedisClient {
         this.port = port;
     }
 
+    /**
+     * Create connection to redis server
+     *
+     * @return boolean
+     */
     public boolean connect() {
         try {
             this.jedis = new Jedis(this.host, this.port);
@@ -28,12 +33,36 @@ public class RedisClient {
         return isConnected;
     }
 
+    /**
+     * Add to a queue, create queue if not exists
+     *
+     * @param queueName The queue name i.e batch-<batch id>
+     * @param value     The chunk data need to be processed
+     */
     public void addToQueue(String queueName, String value) {
         this.jedis.lpush(queueName, value);
     }
 
+    /**
+     * FIFO from the batch queue
+     *
+     * @param queueName The queue name i.e batch-<batch id>
+     * @return boolean
+     */
     public String popFromQueue(String queueName) {
         return this.jedis.rpop(queueName);
+    }
+
+    public void addKeyValue(String key, String value) {
+        this.jedis.set(key, value);
+    }
+
+    public String getKeyValue(String key) {
+        return this.jedis.get(key);
+    }
+
+    public long deleteKeyValue(String key) {
+        return this.jedis.del(key);
     }
 
     public void setConnected(boolean connected) {
